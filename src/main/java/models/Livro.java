83333c7569a081d5c818e3d8b74ff4f0281ed1d2
Beatriz Medeiros <beatrizmedeiros234.bm@gmail.com>
2023-05-10 23:59:08 +0000

    finalizando a classe livro

--- src/main/java/models/Livro.java
+++ src/main/java/models/Livro.java
@@ -1,11 +1,54 @@
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Id;
-
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Table(name = "livros")
 
 public class Livro{
-    
+
+    private int id;
+    private String titulo;
+
+public void setNome(String titulo){
+    this.titulo = titulo;
 }
+
+public String getNome(){
+    return titulo;
+}
+
+public void serId(int id){
+    this.id = id;
+}
+
+public int getId(){
+    return id;
+}
+
+@ManyToOne
+@JoinColumn(name = "id_autor")
+private Autor autor;
+
+public void setAutor(Autor autor){
+    this.autor = autor;
+}
+
+public Autor getAutor(){
+    return autor
+}
+
+@ManyToOne
+@JoinColumn(name = "id_genero")
+private Genero genero;
+public void SetGenero (Genero genero){
+    this.genero = genero;
+}
+public Genero getGenero(){
+    return genero;
+}
+
+
+} 
